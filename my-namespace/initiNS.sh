db2b534ba81130ed3f94604797959670671c76c2
ablouza <akram.blouza@externe.mousquetaires.com>
2018-04-16 18:30:56 +0200

    rename files and script to create templates

---
+++ my-namespace/initiNS.sh
@@ -0,0 +1,21 @@
+namsespace_name=$1
+requests_cpu=$2
+requests_memory=$3
+requests_storage=$4
+limits_cpu=$5
+limits_memory=$6
+gold_requests_storage=$7
+silver_requests_storage=$8
+bronze_requests_storage=$9
+
+echo "Initialiser les resources au namespace $namsespace_name"
+oc process -f namespace-resources-template.yaml \
+-p requests_cpu=$requests_cpu \
+-p requests_memory=$requests_memory \
+-p requests_storage=$requests_storage \
+-p limits_cpu=$limits_cpu \
+-p limits_memory=$limits_memory \
+-p gold_requests_storage=$gold_requests_storage \
+-p silver_requests_storage=$silver_requests_storage \
+-p bronze_requests_storage=$bronze_requests_storage \
+-n $namsespace_name | oc create -f -
