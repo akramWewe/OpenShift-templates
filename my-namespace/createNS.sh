c57a3f70785b45eaa4fdac08814f299e07740aa3
ablouza <akram.blouza@externe.mousquetaires.com>
2018-04-16 13:09:48 +0200

    add template my-quota and script shell to create a new namespace with these quota

---
+++ my-namespace/createNS.sh
@@ -0,0 +1,10 @@
+namsespace_name=$1
+echo "1- Creation du namespace"
+oc create new-project $namsespace_name
+
+echo "2- Creation du la resource quotas"
+oc project $namsespace_name
+oc create -f my-quotas.yaml -n $namsespace_name
+oc process my-quotas -n $namsespace_name | oc create -f -
+
+
