0ffd2f3ce27543e4adb1c06a91b3788a1233dab3
root <root@l203ri3009.mousquetaires.com>
2018-04-16 18:41:07 +0200

    add quota

--- my-namespace/createNS.sh
+++ my-namespace/createNS.sh
@@ -1,6 +1,8 @@
 namsespace_name=$1
 label_name=$2
 
+oc project default
+
 echo "Creation du namespace $namespace_name"
 oc process -f my-namespace.yaml -p name_namespace=$namsespace_name -p label=$label_name -n default | oc create -f -
 
