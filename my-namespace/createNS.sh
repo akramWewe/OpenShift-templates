c93217342b07b33894a870d7e1f8f7f06c89a314
ablouza <akram.blouza@externe.mousquetaires.com>
2018-04-17 13:52:23 +0200

    add selector node in namespace template

--- my-namespace/createNS.sh
+++ my-namespace/createNS.sh
@@ -1,8 +1,15 @@
 namsespace_name=$1
-label_name=$2
+environnement=$2
+site=$3
+zone=$4
 
 echo "Creation du namespace $namespace_name"
-oc process -f my-namespace.yaml -p name_namespace=$namsespace_name -p label=$label_name -n default | oc create -f -
+oc project default
+oc process -f my-namespace.yaml -p name_namespace=$namsespace_name \
+-p environnement=$environnement \
+-p site=$site \
+-p zone=$zone \
+-n default | oc create -f -
 
 
 
